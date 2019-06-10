b5942ba6abb4678d34213e33b6eedf43ec8376d0
Mohammad Sorouri <sorouri1380@gmail.com>
2019-06-10 20:21:48 +0430

    add home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "flutter Ui Designing",
       theme: ThemeData(
+        fontFamily: "Roboto",
         primarySwatch: Colors.grey,
       ),
       home: MyHomePage(),
@@ -26,6 +27,114 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Color(0xff18d191),
+                    borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Icon(Icons.local_offer,color: Colors.white,),
+                ),
+                Container(
+                  margin: EdgeInsets.only(right: 50,top: 50),
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xfffc6a7f),
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Icon(Icons.home,color: Colors.white,),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 30,top: 50),
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xffffce56),
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Icon(Icons.local_car_wash,color: Colors.white,),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 90,top: 40),
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xff45e0ec),
+                      borderRadius: BorderRadius.circular(50)
+                  ),
+                  child: Icon(Icons.place,color: Colors.white,),
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 8,bottom: 80),
+                  child:   Text("Quick Bee",style: TextStyle(fontSize: 30,),),
+                ),
+
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 30,right: 30,top: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Color(0xff18d191),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Text("Sign In With Email",style: TextStyle(fontSize: 20,color: Colors.white),),
+                  ) ,
+                ),
+
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 30,right: 5,top: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Color(0xff4364a1),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Text("FaceBook",style: TextStyle(fontSize: 20,color: Colors.white),),
+                  ) ,
+                ),
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    margin: EdgeInsets.only(left: 5,right: 30,top: 10),
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Color(0xffdf5138),
+                        borderRadius: BorderRadius.circular(10)
+                    ),
+                    child: Text("Google",style: TextStyle(fontSize: 20,color: Colors.white),),
+                  ) ,
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
